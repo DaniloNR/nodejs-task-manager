67154c4a4a0c165d23ab82f24e96f9608f66bdb6
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-02-17 16:38:06 -0300

    fix: make server run on basic settings at first

--- src/server.js
+++ src/server.js
@@ -2,18 +2,9 @@ import http from "node:http";
 import { json } from "./middlewares/json.js";
 
 const server = http.createServer(async (req, res) => {
-  const { method, url } = req;
   await json(req, res);
-  const route = routes[method].find((route) => route.path.test(url));
 
-  if (route) {
-    const routeParams = req.url.match(route.path);
-    const { query, ...params } = routeParams.groups;
-
-    req.params = params;
-    req.query = query ? extractQueryParams(query) : {};
-    return route.handler(req, res);
-  }
+  res.writeHead(200).end();
 
   return res.writeHead(404).end();
 });
