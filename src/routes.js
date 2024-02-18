efc1c1acd97641baac5a8645d6ccdd17b0e69dc7
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-02-18 15:41:40 -0300

    feat: add all routes

---
+++ src/routes.js
@@ -0,0 +1,77 @@
+import { randomUUID } from "node:crypto";
+import { Database } from "./database.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
+
+const database = new Database();
+
+export const routes = {
+  GET: [
+    {
+      path: buildRoutePath("/tasks"),
+      handler: (req, res) => {
+        const { search = null } = req.query;
+
+        const tasks = database.select("tasks", search, "title", "description");
+
+        return res.end(JSON.stringify(tasks));
+      },
+    },
+  ],
+  POST: [
+    {
+      path: buildRoutePath("/tasks"),
+      handler: (req, res) => {
+        const { title, description } = req.body;
+
+        const task = {
+          id: randomUUID(),
+          title,
+          description,
+          completed_at: null,
+          created_at: new Date().toISOString(),
+          updated_at: new Date().toISOString(),
+        };
+
+        database.insert("tasks", task);
+
+        return res.writeHead(201).end();
+      },
+    },
+  ],
+  PUT: [
+    {
+      path: buildRoutePath("/tasks/:id"),
+      handler: ({ params: { id }, body: { title, description } }, res) => {
+        database.update("tasks", id, {
+          title,
+          description,
+          updated_at: new Date().toISOString(),
+        });
+
+        return res.writeHead(204).end();
+      },
+    },
+  ],
+  PATCH: [
+    {
+      path: buildRoutePath("/tasks/:id/complete"),
+      handler: ({ params: { id } }, res) => {
+        database.update("tasks", id, {
+          completed_at: new Date().toISOString(),
+        });
+        return res.writeHead(204).end();
+      },
+    },
+  ],
+  DELETE: [
+    {
+      path: buildRoutePath("/tasks/:id"),
+      handler: (req, res) => {
+        const { id } = req.params;
+        database.delete("tasks", id);
+
+        return res.writeHead(204).end();
+      },
+    },
+  ],
+};
